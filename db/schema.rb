15ac7d7587f07850bc2ee35fe401f16ebc78a3b3
Ziad Alezzi <lucirieko@gmail.com>
2026-06-03 09:17:23 +0300

    added inventory count to db and form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_29_063201) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_061240) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
